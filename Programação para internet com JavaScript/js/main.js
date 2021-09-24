bba8267d138339e1bd62389d8581e6c528f1c514
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-09-24 14:45:46 -0300

    Array e Dicionario

--- Programação para internet com JavaScript/js/main.js
+++ Programação para internet com JavaScript/js/main.js
@@ -1,9 +1,32 @@
-var nome = "Rafael";
-var idade = 25;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo";
-//alert(nome + "tem" + idade + "anos"); //gera um popup
-//alert(idade + idade2);
-console.log(nome); //aparece a informação no Console do Navegador
-console.log(idade + idade2);
-console.log(frase.replace("Japão", "Brasil"));
+
+//Dicionario
+var frutas = [{ nome: "maça", cor: "vermelha" }, { nome: "uva", cor: "roxa" }]
+console.log(frutas);
+alert(frutas[1].nome);
+
+// var fruta = { nome: "maça", cor: "vermelha" } //parecido com o json
+// console.log(fruta);
+
+
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva"); //adiciona um item a lista
+//lista.pop(); //pop tira o ultimo item da lista
+//console.log(lista);
+//alert(lista[1]);
+//console.log(lista.join(" - "));
+
+
+
+
+
+
+// var nome = "Rafael";
+// var idade = 25;
+// var idade2 = 10;
+// var frase = "Japão é o melhor time do mundo";
+// //alert(nome + "tem" + idade + "anos"); //gera um popup
+// //alert(idade + idade2);
+// console.log(nome); //aparece a informação no Console do Navegador
+// console.log(idade + idade2);
+// console.log(frase.replace("Japão", "Brasil"));
