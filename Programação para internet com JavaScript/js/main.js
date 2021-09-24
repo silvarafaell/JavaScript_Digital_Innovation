7db05011a46081deb659b017e293d3248f88c795
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-09-24 16:49:56 -0300

    Desenvolva paginas web com javascript

--- Programação para internet com JavaScript/js/main.js
+++ Programação para internet com JavaScript/js/main.js
@@ -1,6 +1,33 @@
 
-var d = new Date();
-alert(d.getDate());
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+function validaIdade(idade) {
+    var validar;
+    if (idade >= 18) {
+        validar = true
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));
+
+//alert(soma(5, 10));
+
+
+
+// function setReplace(frase, nome, novo_nome) {
+//     return frase.replace(nome, novo_nome)
+// }
+
+// alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+// var d = new Date();
+// alert(d.getDate());
 
 
 // var count;
