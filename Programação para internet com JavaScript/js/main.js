665fde67a4c8c130c5f2a92a5d155d4f73ecd8dd
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-09-24 14:24:06 -0300

    Introducao ao JavaScript

---
+++ Programação para internet com JavaScript/js/main.js
@@ -0,0 +1,9 @@
+var nome = "Rafael";
+var idade = 25;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + "tem" + idade + "anos"); //gera um popup
+//alert(idade + idade2);
+console.log(nome); //aparece a informação no Console do Navegador
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
