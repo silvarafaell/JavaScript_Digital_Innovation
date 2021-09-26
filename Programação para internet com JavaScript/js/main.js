366587e2c19862d25eef205968bdcaafa7113fe9
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-09-26 20:58:00 -0300

    Manipulando elementos da pagina

--- Programação para internet com JavaScript/js/main.js
+++ Programação para internet com JavaScript/js/main.js
@@ -1,20 +1,43 @@
-
-function soma(n1, n2) {
-    return n1 + n2;
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "<br>Obrigado por clicar</br>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
 }
 
-function validaIdade(idade) {
-    var validar;
-    if (idade >= 18) {
-        validar = true
-    } else {
-        validar = false;
-    }
-    return validar;
+function redirecionar() {
+    window.open("https://ge.globo.com/");
+    //window.location.href = "https://ge.globo.com/";
 }
 
-var idade = prompt("Qual sua idade");
-console.log(validaIdade(idade));
+function trocar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+
+
+// function soma(n1, n2) {
+//     return n1 + n2;
+// }
+
+// function validaIdade(idade) {
+//     var validar;
+//     if (idade >= 18) {
+//         validar = true
+//     } else {
+//         validar = false;
+//     }
+//     return validar;
+// }
+
+// var idade = prompt("Qual sua idade");
+// console.log(validaIdade(idade));
 
 //alert(soma(5, 10));
 
